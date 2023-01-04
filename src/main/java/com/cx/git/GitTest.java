0bccf0d0b0056069340d383604fa3c4259fc05c2
cx <cx@qq.com>
2023-01-04 09:43:23 +0800

    second commit

--- src/main/java/com/cx/git/GitTest.java
+++ src/main/java/com/cx/git/GitTest.java
@@ -3,5 +3,6 @@ package com.cx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("this is git!");
+        System.out.println("this is new git！");
     }
 }
